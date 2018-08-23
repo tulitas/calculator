5b8722eff9ddba5b1cf27edfa37b6797faafbea6
Sergej <tulitas1@gmail.com>
2018-08-23 20:28:31 +0300

    update calculate

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,49 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        double a = Double.parseDouble(expression[0]);
+        double b = Double.parseDouble(expression[2]);
+
+        double result;
+        switch (expression[1]) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+
+
+            default:
+                return "ERROR";
+        }
+        if (expression.length < 4){
+            return String.valueOf(result);
+        }
+        double c = Double.parseDouble(expression[4]);
+        switch (expression[3]) {
+            case "+":
+                result = result + c;
+                break;
+            case "-":
+                result = result - c;
+                break;
+            case "*":
+                result = result * c;
+                break;
+            case "/":
+                result = result / c;
+                break;
+
+
+        }
+        return String.valueOf(result);
     }
 
 }
