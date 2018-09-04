5263aceb97745496167b98ff7674ae674d08195e
Sergej <tulitas1@gmail.com>
2018-09-04 18:42:50 +0300

    update calculate 2

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,28 +1,56 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
  * @version 1.0
  * @since 1.0.0
  */
 class Calculator {
-    //bn
+
     String calculate(String[] expression) {
 
-        double result = Double.parseDouble(expression[0]);
+        List<String> tmp = new ArrayList<>();
+
+        double a = Double.parseDouble(expression[0]);
         for (int i = 1; i < expression.length; i += 2) {
             String op = expression[i];
             double b = Double.parseDouble(expression[i + 1]);
-            switch (op){
+            switch (op) {
+                case "+":
+                case "-":
+                    tmp.add(String.valueOf(a));
+                    tmp.add(op);
+                    a = b;
+                    break;
+                case "*":
+                    a *= b;
+                    break;
+                case "/":
+                    a /= b;
+                    break;
+            }
+        }
+        tmp.add(String.valueOf(a));
+        System.out.println(tmp);
+
+
+        double result = Double.parseDouble(tmp.get(0));
+        for (int i = 1; i < tmp.size(); i += 2) {
+            String op = tmp.get(i);
+            double b = Double.parseDouble(tmp.get(i + 1));
+            switch (op) {
                 case "+":
                     result += b;
                     break;
                 case "-":
                     result -= b;
                     break;
-                    default:
-                        return "ERROR";
-}
+                default:
+                    return "ERROR";
+            }
         }
 
         return String.valueOf(result);
